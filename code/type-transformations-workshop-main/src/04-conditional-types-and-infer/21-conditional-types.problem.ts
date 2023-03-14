aa607b4fffada62ad32f12d284e55da48a5942de
Dmitry Khodyrev <dooommm@mail.ru>
2023-03-14 19:29:23 +0800

    first commit

---
+++ code/type-transformations-workshop-main/src/04-conditional-types-and-infer/21-conditional-types.problem.ts
@@ -0,0 +1,9 @@
+import { Equal, Expect } from '../helpers/type-utils';
+
+type YouSayGoodbyeAndISayHello<T extends 'hello' | 'goodbye'> =
+  T extends 'goodbye' ? 'hello' : 'goodbye';
+
+type tests = [
+  Expect<Equal<YouSayGoodbyeAndISayHello<'hello'>, 'goodbye'>>,
+  Expect<Equal<YouSayGoodbyeAndISayHello<'goodbye'>, 'hello'>>
+];
